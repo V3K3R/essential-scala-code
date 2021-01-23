401b21c8c6b8b2d0011fb57c07b78db739916068
Dheker Labidi <dheker.labidi@z-lab.com>
2021-01-23 15:48:35 +0100

    ex2 home work

--- src/main/scala/part1/Exercise2Expressions.scala
+++ src/main/scala/part1/Exercise2Expressions.scala
@@ -6,16 +6,17 @@ object Exercise2Expressions {
   }
 
   def greeting(name: String): String = {
-    ???
+    // "Hello " + name + "!"
+    s"Hello $name!"
   }
 
   // Hint - You can reverse a string with `string.reverse`
   def palindrome(str: String): Boolean = {
-    ???
+    str.reverse == str
   }
 
   def factorial(n: Int): Int = {
-    ???
+    if (n > 1) n * factorial(n - 1) else 1
   }
 
   def main(args: Array[String]): Unit = {
